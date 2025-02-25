d1f2ed6e2e3a1300be39f3d13b8a96744c62d3b7
Saturn Computing Topic <hatsanmobiletopic@gmail.com>
2025-02-25 04:49:35 -0800

    Add files via upload

---
+++ Plainrock124 Simulator.exe_Data/imported/scripts/Plainrock.js
@@ -0,0 +1,12 @@
+let HP = 15;
+
+if (HP > 0) {
+    console.log("HP 15");
+}else{
+    console.log("let goooo");
+}
+if (HP < 20) {
+    console.log("HP 15");
+}else{
+    console.log("let goooo");
+}
